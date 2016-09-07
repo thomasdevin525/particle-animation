13fd9626d1105d8913e7b6cc7913d235929248ee
Thomas Devin <thomasdevin525@yahoo.com>
2016-09-06 18:41:59 -0700

    Added onEmitterDeath function and comment

--- js/index.js
+++ js/index.js
@@ -10,10 +10,12 @@ easeParticlesModule.create(p);
 p.addEventListener('click', function (e) { 
     
     // Creating an emitter whenever the user clicks on the particles canvas
+    // The createEmitter can take the 6 properties listed below
     easeParticlesModule.createEmitter({ 
         position: { x: e.pageX, y: e.pageY },
         destination: { x: 0, y: 0 },
         onParticleDeath: function (a) { var c = document.getElementById('counter'); c.innerHTML = parseInt(c.innerHTML) + a; },
+        onEmitterDeath: function () {},
         color: '#000000',
         sampleAmount: 100
     });
