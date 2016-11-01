472bb6ab81eea6f01472058c83839081636bec5e
Thomas Devin <thomasdevin525@yahoo.com>
2016-11-01 11:47:05 -0700

    Cleaned and commented

--- js/particle-animation.js
+++ js/particle-animation.js
@@ -1,141 +1,249 @@
 var easeParticlesModule = (function () {
     
     var data = {
-        
-        emitters: [],
-        
-        /* Change max if you'd like to see more particles */
-        maxParticlesPerEmitter: 25,
-        
-        /* Chosen easing equations from easing-equations.js */
-        easings: ['linear', 'inQuad', 'outQuad', 'inOutQuad', 'inSine', 'outSine', 'inOutSine', 'inExpo', 'outExpo', 'inOutExpo', 'inCirc', 'outCirc', 'inOutCirc'],
-        
-        /* Defined on create(), holds references to canvas and context */
-        canvas: null,
-        ctx: null,
-        
-        /* Looping is true while there are active particles */
-        looping: true
-        
+      
+      // Edit to see more particles with greater sample amounts
+      maxParticlesPerEmitter: 25,
+      
+      // Edit property names taken from easingData
+      easings: ['linear', 'inQuad', 'outQuad', 'inOutQuad', 'inSine', 'outSine', 'inOutSine', 'inExpo', 'outExpo', 'inOutExpo', 'inCirc', 'outCirc', 'inOutCirc'],
+      
+      // Don't worry about these
+      looping: true,
+      canvas: null,
+      ctx: null,
+      emitters: [],
+      
     };
     
-    function Emitter (pos, des, opd, oed, sam, col) {
+    function Emitter ( pos, des, opd, oed, sam, col ) {
+      
+      // Define position and desitination
+      this.position = pos || {x: 0, y: 0};
+      this.particleDestination = des || {x: 0, y: 0};
+      
+      // Total point amount and define number of particles
+      this.sampleAmount = sam || 1;
+      this.killAfterParticle = sam > data.maxParticlesPerEmitter ? data.maxParticlesPerEmitter : this.sampleAmount;
+      this.rate = this.killAfterParticle;
+      
+      // When particle and emitter dies
+      this.onParticleDeath = opd || function () {};
+      this.onEmitterDeath = oed || function () {};
+      
+      // Color and particles
+      this.particleColor = col || '#FFFFFF';
+      this.particleCount = 0;
+      this.particles = [];
+
+      // Determine the sample amount for each particle
+      this.getParticleSampleAmount = function () {
         
-        /* The start and end position of each particle */
-        this.position = pos || {x: 0, y: 0};
-        this.particleDestination = des || {x: 0, y: 0};
+        var amount = Math.round(this.sampleAmount / this.particles.length);
+        this.sampleAmount -= amount;
+        return amount;
         
-        this.sampleAmount = sam || 1;
-        this.killAfterParticle = sam > data.maxParticlesPerEmitter ? data.maxParticlesPerEmitter : this.sampleAmount;
-        this.rate = this.killAfterParticle;
-        this.onParticleDeath = opd || function () {};
-        this.onEmitterDeath = oed || function () {};
-        this.particleColor = col || '#FFFFFF';
-        this.particleCount = 0;
-        this.particles = [];
+      };
+      
+      // Spawn particles for this emitter
+      this.spawnParticles = function () {
         
-        this.getParticleSampleAmount = function () {
-            var amount = Math.round(this.sampleAmount / this.particles.length);
-            this.sampleAmount -= amount;
-            return amount;
-        };
+        var i = 0,
+            len = this.rate,
+            o = data.easings,
+            x, 
+            y, 
+            f;
         
-        this.spawnParticles = function () {
-            var i = 0, x = 0, y = 0, f = 0,
-                o = data.easings;
-            for (; i < this.rate; i++) {
-                do {
-                    x = Math.floor(Math.random() * o.length);
-                    y = Math.floor(Math.random() * o.length);
-                } while (x === y);
-                f = Math.floor(Math.random() * 15) + 30;
-                this.particles.push(new Particle(this.position, o[x], o[y], f));
-                this.particleCount++;
-            }
-        };
+        // Run through the rate for this frame
+        for (; i < len; i++) {
+          
+          // Make sure the x and y easing equations are different
+          do {
+            
+            x = Math.floor( Math.random() * o.length );
+            y = Math.floor( Math.random() * o.length );
+            
+          } while ( x === y );
+          
+          // Random frame amount between 15 and 45
+          f = Math.floor( Math.random() * 15 ) + 30;
+          
+          // Create the particle and inc particle count
+          this.particles.push( new Particle( this.position, o[x], o[y], f ) );
+          this.particleCount++;
+          
+        }
         
+      };
+      
     }
-    function Particle (position, easeX, easeY, frames) {
-        this.frame = 0;
-        this.frames = frames || 30;
-        this.position = position || {x: 0, y: 0};
-        this.easeX = easeX || 'linear';
-        this.easeY = easeY || 'linear';
+    function Particle ( position, easeX, easeY, frames ) {
+      
+      // Define particle properties
+      this.frame = 0;
+      this.frames = frames || 30;
+      this.position = position || {x: 0, y: 0};
+      this.easeX = easeX || 'linear';
+      this.easeY = easeY || 'linear';
+      
     }
         
     function addParticles () {
-        var i = 0;
-        for (; i < data.emitters.length; i++) {
-            if (data.emitters[i].particleCount < data.emitters[i].killAfterParticle) {
-                data.emitters[i].spawnParticles();
-            }
+      
+      var i = 0,
+          emts = data.emitters,
+          len = emts.length;
+      
+      // Run through the emitters
+      for ( ; i < len; i++ ) {
+        
+        // Make sure the emitter hasn't reached it's max particle count
+        if ( emts[i].particleCount < emts[i].killAfterParticle ) {
+          
+          // Call to spawn more particles
+          emts[i].spawnParticles();
+          
         }
+        
+      }
+      
     }
     function moveParticles () {
-        var i = 0, j = 0, e = {}, p = {}, x = 0, y = 0, mx = 0, my = 0, nx = 0, ny = 0;
-        for (; i < data.emitters.length; i++) {
-            e = data.emitters[i];
-            x = e.position.x;
-            y = e.position.y;
-            mx = e.particleDestination.x - e.position.x;
-            my = e.particleDestination.y - e.position.y;
-            j = e.particles.length - 1;
-            for (; j >= 0; j--) {
-                p = e.particles[j];
-                if (p.frame < p.frames) {
-                    p.frame++;
-                    nx = easingData[p.easeX](p.frame, x, mx, p.frames);
-                    ny = easingData[p.easeY](p.frame, y, my, p.frames);
-                    p.position = {x: nx, y: ny};
-                } else {
-                    e.onParticleDeath(e.getParticleSampleAmount());
-                    e.particles.splice(j, 1);
-                }
-            }
+      
+      // Define variables
+      var i = 0, 
+          emts = data.emitters,
+          len = emts.length,
+          j, 
+          e, 
+          p,
+          mx,
+          my,
+          nx,
+          ny;
+
+      for (; i < len; i++) {
+
+        e = emts[i];
+        
+        // Get x and y move position
+        mx = e.particleDestination.x - e.position.x;
+        my = e.particleDestination.y - e.position.y;
+        
+        j = e.particles.length - 1;
+
+        // Loop backwards through the particles for splicing
+        for (; j >= 0; j--) {
+
+          p = e.particles[j];
+
+          // Check the frame count of the particle
+          if ( p.frame < p.frames ) {
+
+            // Inc frame count
+            p.frame++;
+            
+            // Get new x and y position of the particle
+            nx = easingData[p.easeX]( p.frame, e.position.x, mx, p.frames );
+            ny = easingData[p.easeY]( p.frame, e.position.y, my, p.frames );
+            
+            // Set the new position
+            p.position = { x: nx, y: ny };
+
+          } else {
+
+            // Call particle death function and remove it from the array
+            e.onParticleDeath( e.getParticleSampleAmount() );
+            e.particles.splice( j, 1 );
+
+          }
+
         }
+
+      }
+      
     }
     function drawParticles () {
-        var i = 0, j = 0, p = {}, c = '';
-        for (; i < data.emitters.length; i++) {
-            j = 0;
-            for (; j < data.emitters[i].particles.length; j++) {
-                p = data.emitters[i].particles[j];
-                data.ctx.beginPath();
-                data.ctx.arc(p.position.x, p.position.y, 5, 0, 2*Math.PI);
-                data.ctx.fillStyle = data.emitters[i].particleColor;
-                data.ctx.fill();
-                data.ctx.closePath();
-            }
+      
+      var i = 0,
+          emts = data.emitters,
+          len = emts.length,
+          j,
+          p,
+          c;
+      
+      // Loop the emitters
+      for ( ; i < len; i++ ) {
+                
+        // Loop each particle
+        for ( j = 0; j < emts[i].particles.length; j++ ) {
+          
+          p = emts[i].particles[j];
+          
+          // Draw the particle on the screen
+          data.ctx.beginPath();
+          data.ctx.arc( p.position.x, p.position.y, 5, 0, 2*Math.PI );
+          data.ctx.fillStyle = emts[i].particleColor;
+          data.ctx.fill();
+          data.ctx.closePath();
+          
         }
+        
+      }
+      
     }
     function killEmitters () {
-        var i = data.emitters.length - 1;
-        for (; i >= 0; i--) {
-            if (data.emitters[i].particles.length === 0) {
-                data.emitters.splice(i, 1);
-            }
+      
+      var emts = data.emitters,
+          i = emts.length - 1;
+      
+      // Loop emitters
+      for ( ; i >= 0; i-- ) {
+        
+        // Check if the emitter has any particles
+        if ( emts[i].particles.length === 0 ) {
+          
+          // Remove it
+          emts.splice( i, 1 );
+          
         }
-        if (data.emitters.length === 0) {
-            data.looping = false;
-        }
+        
+      }
+      
+      // If no emitters then disable looping
+      if ( emts.length === 0 ) data.looping = false;
+      
     }
-                           
-    function create (canvas) {
-        if (canvas === null) return;
+    
+    return {
+      
+      create: function ( canvas ) {
+
+        if ( canvas == undefined ) return;
+
+        // Grab the pixel ratio and scale
         var res = window.devicePixelRatio || 1,
             scale = 1 / res;
-        
+
+        // Save the canvas and context
         data.canvas = canvas;
         data.ctx = data.canvas.getContext('2d');
-    
+
+        // Set the width and height properties
         data.canvas.width  = window.innerWidth * res;
         data.canvas.height = window.innerHeight * res;
         data.canvas.style.width = window.innerWidth + 'px';
         data.canvas.style.height = window.innerHeight + 'px';
 
+        // Set the scale
         data.ctx.scale(res, res);
-    }
-    function createEmitter (preferences) {
+
+      },
+      
+      createEmitter: function ( preferences ) {
+      
+        // Save preference variables
         var p = preferences || {},
             pos = {x: p.position.x, y: p.position.y},
             des = {x: p.destination.x, y: p.destination.y},
@@ -143,23 +251,32 @@ var easeParticlesModule = (function () {
             oed = p.onEmitterDeath || function () {},
             sam = p.sampleAmount || 1,
             col = p.color || '#FFFFFF';
-        data.emitters.push(new Emitter(pos, des, opd, oed, sam, col));
+
+        // Create and add the emitter
+        data.emitters.push( new Emitter( pos, des, opd, oed, sam, col ) );
+
+        // Start looping
         data.looping = true;
-    }
-    function loop() {
-        if (data.looping) {
-            addParticles();
-            killEmitters();
-            moveParticles();
-            data.ctx.clearRect(0, 0, data.canvas.width, data.canvas.height);
-            drawParticles();
+
+      },
+      
+      loop: function () {
+      
+        // Make sure there are emitters active
+        if ( data.looping ) {
+
+          // Clear canvas
+          data.ctx.clearRect(0, 0, data.canvas.width, data.canvas.height);
+
+          addParticles();
+          killEmitters();
+          moveParticles();
+          drawParticles();
+
         }
-    }
-    
-    return {
-        create: create,
-        createEmitter: createEmitter,
-        loop: loop
+
+      }
+      
     }
     
 })();
